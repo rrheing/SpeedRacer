c6dc547eda8e3376cee64ada1a1cea5f543826a0
ubcs-io <roman@ubcs.io>
2016-10-24 20:50:51 -0400

    Added app.js file framework
    Javascript comments for later page actions

---
+++ app.js
@@ -0,0 +1,10 @@
+/*   This is where the JS for page / section selection will go    */
+
+
+// Option to select language
+
+
+// Option to select Focus Area content
+
+
+// Highlight center buttons and draw attention to action
